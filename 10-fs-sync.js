eee8bb7679e03446f00b8c7ab4bfb7baf7a2beda
Andrey Mattos <andreysmattos@hotmail.com>
2022-11-07 08:33:25 -0300

    fix

---
+++ 10-fs-sync.js
@@ -0,0 +1,10 @@
+const {readFileSync, writeFileSync} = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf-8');
+const second = readFileSync('./content/first.txt', 'utf-8');
+
+console.log('first', first);
+console.log('second', second);
+
+// writeFileSync('./content/result.txt', `Here is the result: ${first}, ${second}`);
+writeFileSync('./content/result.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'});
