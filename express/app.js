a8d99107bbf38983a9e5665527a70206670652ce
Andrey Mattos <andreysmattos@hotmail.com>
2022-11-17 08:21:11 -0300

    http

--- express/app.js
+++ express/app.js
@@ -1,1 +1,16 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        'content-type': 'image/jpeg'
+    });
+
+    res.write('<h1> Home Page </h1>');
+
+    res.end();
+});
+
+server.listen("4477", function () {
+  console.log("Server start");
+});
