2b3b001c3fce8d704ca04f45f24c6bfd41255a3e
Andrey Mattos <andreysmattos@hotmail.com>
2022-11-17 08:22:32 -0300

    fix

--- express/app.js
+++ express/app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
 
     res.writeHead(200, {
-        'content-type': 'image/jpeg'
+        'content-type': 'text/plain'
     });
 
     res.write('<h1> Home Page </h1>');
