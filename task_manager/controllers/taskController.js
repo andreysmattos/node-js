ef2d1b579b059edb4e1c00050901150a3b18f05b
Andrey Mattos <andreysmattos@hotmail.com>
2022-11-21 11:36:06 -0300

    fix

--- task_manager/controllers/taskController.js
+++ task_manager/controllers/taskController.js
@@ -1,12 +1,27 @@
-const get = (req, res) => {
+const index = (req, res) => {
   res.status(200).json([1, 2, 3]);
 };
 
 const store = (req, res) => {
-  res.status(201).json([1, 2, 3]);
+  res.status(201).json(req.body);
+};
+
+const show = (req, res) => {
+  res.status(200).json(req.params);
+};
+
+const update = (req, res) => {
+  res.status(200).json([req.params, req.body]);
+};
+
+const destroy = (req, res) => {
+  res.status(200).json(req.params);
 };
 
 module.exports = {
-  get,
+  index,
   store,
+  show,
+  update,
+  destroy,
 };
--- task_manager/routes/api/task.js
+++ task_manager/routes/api/task.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../../controllers/taskController");
 
-router.get("/", taskController.get);
+router.get("/", taskController.index);
 router.post("/", taskController.store);
+
+router.get("/:id", taskController.show);
+router.patch("/:id", taskController.update);
+router.delete("/:id", taskController.destroy);
 
 module.exports = router;
