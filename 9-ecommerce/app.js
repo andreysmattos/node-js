6313a5ad683038d442d062c2fb64c3170a6d1ea8
Andrey Mattos <andreysmattos@hotmail.com>
2022-12-30 12:35:04 -0300

    fix

--- 9-ecommerce/app.js
+++ 9-ecommerce/app.js
@@ -1,1 +1,34 @@
-console.log("Express Ecommerce API");
+require("dotenv").config();
+require("express-async-errors");
+
+const morgan = require("morgan");
+const express = require("express");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+
+const connectDB = require("./config/database");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  return res.status(200).send("Work");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server listen on port ${port}`));
+  } catch (error) {
+    console.log("Start error", error);
+  }
+};
+
+start();
--- 9-ecommerce/config/database.js
+++ 9-ecommerce/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+
 const connectDB = (url) => {
   return mongoose.connect(url);
 };
